e38663ccfc30364a007294575930273093a11cb5
Johannes Wolff <johannes.wolff@outlook.com>
2015-06-26 13:49:30 +0200

    Twitter button on detail site of movies added

--- client/js/controller.js
+++ client/js/controller.js
@@ -73,4 +73,23 @@
         });
     });
 
+    app.directive('twitter', [
+        function() {
+            return {
+                link: function(scope, element, attr) {
+                    setTimeout(function() {
+                            twttr.widgets.createShareButton(
+                                attr.url,
+                                element[0],
+                                function(el) {}, {
+                                    count: 'none',
+                                    text: attr.text
+                                }
+                            );
+                    });
+                }
+            }
+        }
+    ]);
+
 })();
