e224edc418efccc3b9ddafe24a6423a89dec429e
Uli Schmidt <uli.schmidt@uni-muenster.de>
2015-06-26 14:10:28 +0200

    Fix indentation

--- client/js/controller.js
+++ client/js/controller.js
@@ -78,17 +78,17 @@
             return {
                 link: function(scope, element, attr) {
                     setTimeout(function() {
-                            twttr.widgets.createShareButton(
-                                attr.url,
-                                element[0],
-                                function(el) {}, {
-                                    count: 'none',
-                                    text: attr.text
-                                }
-                            );
+                        twttr.widgets.createShareButton(
+                            attr.url,
+                            element[0],
+                            function(el) {}, {
+                                count: 'none',
+                                text: attr.text
+                            }
+                        );
                     });
                 }
-            }
+            };
         }
     ]);
 
