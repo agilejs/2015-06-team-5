9b5d13a79add6e74bed58381cfb87c10355e9395
Peter Brüske <p.brueske@gmail.com>
2015-06-26 16:07:29 +0200

    fix ui tests

--- test/client/ui/NewMovie.js
+++ test/client/ui/NewMovie.js
@@ -4,17 +4,32 @@ module.exports = function() {
     this.title = element(by.model('movie.title'));
     this.description = element(by.model('movie.description'));
     this.save = element(by.css('.btn-primary'));
+    //this.ptor = protractor.getInstance();
 
     this.open = function() {
         browser.get('/movies/new');
     };
 
-    this.addMovie = function(title, description) {
+    this.addMovie = function(title, desc) {
         this.open();
         this.title.clear();
         this.title.sendKeys(title);
-        this.description.clear();
-        this.description.sendKeys(description);
+        this.description.click().then(function(){
+            browser.actions().sendKeys(desc).perform();
+        });
         this.save.click();
     };
 };
+
+//var ptor = protractor.getInstance();
+
+//Get the texteditor of commentary
+//expect(element(by.id('commentaryEditorTextArea')).isPresent()).toBe(true);
+//expect(element(by.model('commentary.content')).isPresent()).toBe(true);
+//put the focus
+//element(by.model('commentary.content')).click();
+//send a text: don't use sendkeys of the element.
+//ptor.actions().sendKeys('Prueba comentario').perform();
+//write in console the text put in the previous step:
+//element(by.model('commentary.content')).getText().then(console.log);
+//check if the text has been put.
